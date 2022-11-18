4755c2749b6bd298eb993b834996f117845b4e2e
Serhii <serhii.kambur@unilimes.com>
2022-11-18 16:22:15 -0300

    Fixed double rendering of content frame

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import "./App.css";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { ContentFrame } from "./components/ContentFrame/ContentFrame";
 
 const App = () => {
   const ref = useRef(null);
-  const cssRef = useRef(null);
+  const _three = useRef(null);
+  const _renderer = useRef(null);
+  const _scene = useRef(null);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const sdk = ref.current;
     const showcaseWindow = sdk.contentWindow;
-
-    let cssScene;
-    let renderer2;
-    let camera;
 
     sdk.addEventListener("load", async function () {
       let sdk;
       try {
         sdk = await showcaseWindow.MP_SDK.connect(showcaseWindow);
         const [model] = await sdk.Scene.query(["model"]);
-        const scene = model.parent;
-        camera = scene.getObjectByName("CameraRig").camera;
+        _scene.current = model.parent;
         await sdk.Scene.configure(function (renderer, three) {
-          cssScene = new three.Scene();
-          const element = initDomElement();
-          const domObject = initDomObject(three, element);
-          renderer2 = initCSS3DRenderer(three);
-          cssScene.add(domObject);
+          _three.current = three;
+          _renderer.current = renderer;
+          setLoaded(true);
         });
       } catch (e) {
         console.error(e);
@@ -34,43 +30,8 @@ const App = () => {
       }
     });
 
-    const animate = () => {
-      if (camera && cssScene) {
-        renderer2.render(cssScene, camera);
-      }
-      requestAnimationFrame(animate);
-    };
-    animate();
-
     //TODO: add some more actions using sdk. E.g. sdk.Scene()...
   }, []);
-
-  const initDomElement = () => {
-    const element = document.createElement("iframe");
-    element.style.width = "600px";
-    element.style.height = "380px";
-    element.style.opacity = 0.999;
-    element.src =
-      "https://admin.treedis.com/login?_ga=2.105331530.893087370.1668653164-1072976840.1668653164&_gl=1*umzo4r*_ga*MTA3Mjk3Njg0MC4xNjY4NjUzMTY0*_ga_YLK9Q8X6JM*MTY2ODcwMzQ2OS4zLjAuMTY2ODcwMzQ2OS4wLjAuMA..";
-    return element;
-  };
-
-  const initDomObject = (three, element) => {
-    const domObject = new three.CSS3DObject(element);
-    domObject.scale.set(0.003, 0.003, 0.003);
-    domObject.position.set(5.445350646972656, 1.3341099739074707, 21);
-    domObject.rotation.set(0, Math.PI * 0.96, 0);
-    return domObject;
-  };
-
-  const initCSS3DRenderer = (three) => {
-    const renderer2 = new three.CSS3DRenderer();
-    renderer2.setSize(window.innerWidth, window.innerHeight);
-    renderer2.domElement.style.position = "absolute";
-    renderer2.domElement.style.top = 0;
-    cssRef.current.appendChild(renderer2.domElement);
-    return renderer2;
-  };
 
   return (
     <div className="App">
@@ -81,7 +42,14 @@ const App = () => {
         allowFullScreen
         allow="vr"
       ></iframe>
-      <ContentFrame refProp={cssRef} />
+      <ContentFrame
+        loaded={loaded}
+        three={_three.current}
+        scene={_scene.current}
+        src={
+          "https://admin.treedis.com/login?_ga=2.105331530.893087370.1668653164-1072976840.1668653164&_gl=1*umzo4r*_ga*MTA3Mjk3Njg0MC4xNjY4NjUzMTY0*_ga_YLK9Q8X6JM*MTY2ODcwMzQ2OS4zLjAuMTY2ODcwMzQ2OS4wLjAuMA.."
+        }
+      />
     </div>
   );
 };
--- src/components/ContentFrame/ContentFrame.jsx
+++ src/components/ContentFrame/ContentFrame.jsx
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-export const ContentFrame = ({ refProp }) => {
+export const ContentFrame = ({ loaded, three, scene, src }) => {
+  const cssRef = useRef(null);
+  const _element = useRef(null);
+
+  useEffect(() => {
+    if (loaded && scene && !_element.current) {
+      const camera = scene.getObjectByName("CameraRig").camera;
+      const cssScene = new three.Scene();
+      const element = initDomElement();
+      const domObject = initDomObject(three, element);
+      const renderer2 = initCSS3DRenderer(three);
+      cssScene.add(domObject);
+
+      const animate = () => {
+        if (camera && cssScene) {
+          renderer2.render(cssScene, camera);
+        }
+        requestAnimationFrame(animate);
+      };
+      animate();
+    }
+  }, [loaded]);
+
+  const initDomElement = () => {
+    const element = document.createElement("iframe");
+    _element.current = element;
+    element.style.width = "600px";
+    element.style.height = "380px";
+    element.style.opacity = 0.999;
+    element.src = src;
+    return element;
+  };
+
+  const initDomObject = (three, element) => {
+    const domObject = new three.CSS3DObject(element);
+    domObject.scale.set(0.003, 0.003, 0.003);
+    domObject.position.set(5.445350646972656, 1.3341099739074707, 21);
+    domObject.rotation.set(0, Math.PI * 0.96, 0);
+    return domObject;
+  };
+
+  const initCSS3DRenderer = (three) => {
+    const renderer2 = new three.CSS3DRenderer();
+    renderer2.setSize(window.innerWidth, window.innerHeight);
+    renderer2.domElement.style.position = "absolute";
+    renderer2.domElement.style.top = 0;
+    cssRef.current.appendChild(renderer2.domElement);
+    return renderer2;
+  };
+
   return (
     <div
       style={{
@@ -11,7 +60,7 @@ export const ContentFrame = ({ refProp }) => {
         height: "100%",
         pointerEvents: "none",
       }}
-      ref={refProp}
+      ref={cssRef}
     />
   );
 };
